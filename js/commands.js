d3bfb4060d18cb8b2d2d98f3e0d6178c90c6aee6
TommyYOyoyo <79941840+TommyYOyoyo@users.noreply.github.com>
2022-07-20 16:54:26 -0400

    embed nuke command

--- js/commands.js
+++ js/commands.js
@@ -1,11 +1,13 @@
 // All commands modules
 
+const {Client, Intents, MessageEmbed} = require('discord.js')
+
 require('dotenv').config()
 
-var nukeGifLinks = ["https://media4.giphy.com/media/oe33xf3B50fsc/giphy.gif?cid=ecf05e47zdtvnaelkh2v9kf6prkovyzmz6z37k8vou6y2bnw&rid=giphy.gif&ct=g",
-    "https://media4.giphy.com/media/HhTXt43pk1I1W/200w.webp?cid=ecf05e47jxy10pummvcwov6ash03uu89q52aaziefihqjpvx&rid=200w.webp&ct=g",
-    "https://media2.giphy.com/media/xUPGGePcEyYvkS75ew/200w.webp?cid=ecf05e4708xeboosknn2ym9t9osrr2dtgu5te0qpdlb7lb7y&rid=200w.webp&ct=g",
-    "https://media0.giphy.com/media/XevXoNu5WZxe0/200.webp?cid=ecf05e4708xeboosknn2ym9t9osrr2dtgu5te0qpdlb7lb7y&rid=200.webp&ct=g",
+var nukeGifLinks = ["https://c.tenor.com/giN2CZ60D70AAAAC/explosion-mushroom-cloud.gif",
+    "https://c.tenor.com/YE_vkc5cRxUAAAAM/kaboom-boom.gif",
+    "https://c.tenor.com/h4n4Y_HfjhMAAAAM/haha-bongo-cat.gif",
+    "https://c.tenor.com/EkW1bQdRl2MAAAAC/explodinghehemwhaha.gif",
     "https://media.discordapp.net/attachments/860285099832639491/998418051890425977/unknown.gif",
     "https://media.discordapp.net/attachments/860285099832639491/998418069095448616/unknown.gif"
 ]
@@ -77,18 +79,28 @@ module.exports = {
             place = `${message.author}'s house`
         }
         let randnum = Math.floor(Math.random() * 100)
-        /*if (randnum > 50 && randnum <= 90) {
-            message.reply(`${message.author} dropped a nuke to ${place}, TOTAL DESTRUCTION! \n\nPOV ${place}: ${nukeGifLinks[Math.floor(Math.random() * nukeGifLinks.length)]}`)
-            //message.reply(`POV ${place}: ${nukeGifLinks[Math.floor(Math.random() * nukeGifLinks.length)]}`)
+        let img = undefined
+
+        let reply = ""
+        if (randnum > 50 && randnum <= 90) {
+            reply = `${message.author} dropped a nuke to ${place}, TOTAL DESTRUCTION! \n\nPOV ${place}:`
+            img = `${nukeGifLinks[Math.floor(Math.random() * nukeGifLinks.length)]}`
         } else if (randnum > 20 && randnum < 30) {
-            message.reply(`${message.author} tried to drop a nuke but the Navy intercepted him. **WHAT A KARMA!**`)
+            reply = `${message.author} tried to drop a nuke but the Navy intercepted him. **WHAT A KARMA!**`
         } else if (randnum > 30 && randnum <= 49) {
-            message.reply(`${message.author} dropped the nuke to ${place} but the malprogrammed nuke flew back to his plane. **TO BE CONTINUE...**`)
+            reply = (`${message.author} dropped the nuke to ${place} but the malprogrammed nuke flew back to his plane. **TO BE CONTINUE...**`)
         } else if (randnum > 90 && randnum <= 100) {
-            message.reply(`${message.author} dropped the nuke to ${place} but everyone there magically survived! **MAGIK!** \n\nPOV ${place}: https://c.tenor.com/8gpittE_R9oAAAAM/running-dodging.gif`)
-            //message.reply(`POV ${place}: https://c.tenor.com/8gpittE_R9oAAAAM/running-dodging.gif`)
+            reply = (`${message.author} dropped the nuke to ${place} but everyone there magically survived! **MAGIK!** \n\nPOV ${place}:`)
+            img = "https://c.tenor.com/8gpittE_R9oAAAAM/running-dodging.gif"
         } else {
-            message.reply(`Bro stop dreaming nukes and go get a job man :joy:`)
-        ]*/
+            reply = (`Bro stop dreaming nukes and go get a job man :joy:`)
+        }
+        let nukeMsg = new MessageEmbed()
+        .setColor(`#${Math.floor(Math.random()*16777215).toString(16)}`)
+        .setTitle(`${message.author.username}'s nuking process...`)
+        .setDescription(`${reply}`)
+        .setImage(img)
+
+        message.reply({embeds: [nukeMsg]})
     }
 }
